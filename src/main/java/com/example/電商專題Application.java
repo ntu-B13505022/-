7d0ce201bb11c533b25fd9c0809b8f815c13de58
ntu-B13505022 <950404abc@gmail.com>
2026-06-17 01:35:30 +0800

    Update 電商專題Application.java

--- src/main/java/com/example/電商專題Application.java
+++ src/main/java/com/example/電商專題Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class 電商專題Application {
 
     public static void main(String[] args) {
-        // 🌟 清除未使用的變數宣告，直接執行
+        // 清除未使用的變數宣告，直接執行
         SpringApplication.run(電商專題Application.class, args);
         openHomePage();
     }
@@ -15,7 +15,7 @@ public class 電商專題Application {
     private static void openHomePage() {
         if (java.awt.Desktop.isDesktopSupported()) {
             try {
-                // 🚀 自動幫你導向由 Java 渲染的主網址
+                // 自動導向由 Java 渲染的主網址
                 java.awt.Desktop.getDesktop().browse(new java.net.URI("http://localhost:8081/"));
             } catch (Exception e) {
                 e.printStackTrace();
